9927fba20d1d40d462810b82740389da03c7feb2
cabaeltan28 <rodeltangwapo28@gmail.com>
2026-05-21 09:44:33 +0000

    upgrade na naman

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define file_products "my_products.txt"
 #define file_inventory "my_inventory.txt"
@@ -24,65 +25,87 @@ void retainMoney() {
     }
 }
 
-void initializeFiles();
-void loadProducts(Product products[], int *count);
-void saveProducts(Product products[], int count);
+void check_files();
+void scan_products(Product products[], int *count);
+void update_products(Product products[], int count);
 void displayProducts(Product products[], int count);
 void buyProduct(Product products[], int count, int *money);
 void viewInventory(int money);
 int loadMoney();
 void saveMoney(int money);
-
+void playMiniGame();
+void aboutOwner();
 int main() {
     Product products[10];
     int count = 0;
     int choice;
     int money;
 
-    initializeFiles();
+    check_files();
 
-    loadProducts(products, &count);
+    scan_products(products, &count);
     money = loadMoney();
 
     do {
-        printf("\n===== VENDING MACHINE =====\n");
-        printf("Student Money: PHP %d\n", money);
-        printf("1. View Products\n");
-        printf("2. Buy Product\n");
+        printf("\n===== PBCT VENDING MACHINE =====\n");
+        printf("=== Barya mo. Saya mo. Adelante ===\n");
+        printf("\nStudent Money: PHP %d\n\n", money);
+        printf("1. 🛒Buy Products\n");
+        printf("2. View Product\n");
         printf("3. View Inventory\n");
-        printf("4. Exit\n");
+        printf("4. Play a mini game\n");
+        printf("5. About the owner: \n");
+        printf("6. Exit\n");
         printf("Enter choice: ");
         scanf("%d", &choice);
 
         switch(choice) {
             case 1:
-                displayProducts(products, count);
+                buyProduct(products, count, &money);
+                update_products(products, count);
+                saveMoney(money);
                 break;
 
             case 2:
-                buyProduct(products, count, &money);
-                saveProducts(products, count);
-                saveMoney(money);
+            while(1) {
+                printf("You are now viewing the products.\n");
+                displayProducts(products, count);
+                printf("==============================\n");
+                printf("Tap 1 to go back to main menu:");
+                 int choice;
+                scanf("%d", &choice);
+                if(choice == 1) {
+                 printf("Returning to Main Menu...\n");
+                  break;
+                 } else {
+                  printf("Invalid choice. Returning to Main Menu...\n");
+                 }
+                }
                 break;
 
             case 3:
                 viewInventory(money);
                 break;
-
             case 4:
-                printf("Thank you for using the vending machine!\n");
+                playMiniGame();
+                break;
+             case 5:
+                aboutOwner();
+                break;
+            case 6:
+                printf("Thank you for using the vending machine!Sa uulitin na naman!\n");
                 break;
 
             default:
                 printf("Invalid choice.\n");
         }
 
-    } while(choice != 4);
+    } while(choice != 6);
 
     return 0;
 }
 
-void initializeFiles() {
+void check_files() {
     FILE *fp;
 
     fp = fopen(file_products, "r");
@@ -94,7 +117,7 @@ void initializeFiles() {
         fprintf(fp, "Water 20 15\n");
         fprintf(fp, "Chips 25 12\n");
         fprintf(fp, "Chocolate 30 8\n");
-        fprintf(fp, "Sandwich 50 5\n");
+        fprintf(fp, "Sandwich 50 15\n");
 
         fclose(fp);
     } else {
@@ -121,7 +144,7 @@ void initializeFiles() {
     }
 }
 
-void loadProducts(Product products[], int *count) {
+void scan_products(Product products[], int *count) {
     FILE *fp;
     fp = fopen(file_products, "r");
 
@@ -138,7 +161,7 @@ void loadProducts(Product products[], int *count) {
     fclose(fp);
 }
 
-void saveProducts(Product products[], int count) {
+void update_products(Product products[], int count) {
     FILE *fp;
     fp = fopen(file_products, "w");
 
@@ -153,6 +176,7 @@ void saveProducts(Product products[], int count) {
 }
 
 void displayProducts(Product products[], int count) {
+   
     printf("\n===== AVAILABLE PRODUCTS =====\n");
 if(count == 0) {
         printf("No products available.\n");
@@ -166,12 +190,16 @@ if(count == 0) {
                products[i].price,
                products[i].stock);
     }
+    
 }
 
 void buyProduct(Product products[], int count, int *money) {
+    while(1) {
     int choice;
     FILE *fp;
-
+    printf("\n==============================\n");
+    printf("You are now buying a products.\n");
+    printf("        ||            ||\n");
     displayProducts(products, count);
 
     printf("\nEnter product number to buy: ");
@@ -217,7 +245,24 @@ void buyProduct(Product products[], int count, int *money) {
     }else{
     printf("You bought %d %ss.\n", quantity, products[choice].name);
 }
-    printf("Remaining Money: PHP %d\n", *money);
+    printf("Your money now become: PHP %d\n", *money);
+     int buyMore, timailhan=0;
+    while(buyMore!=1){
+    printf("\nDo you want to buy another product? (1 for Yes, 0 for No): ");
+    scanf("%d", &buyMore);
+    
+    if(buyMore == 0) {
+        timailhan=1;
+        break;
+    }else if(buyMore != 1) {
+        printf("Invalid choice in returning to Main Menu...\n");
+    }
+}
+if(timailhan==0) {
+    printf("Returning to Main Menu...\n");
+    break;
+}
+    }
 }
 
 void viewInventory(int money) {
@@ -258,4 +303,83 @@ void saveMoney(int money) {
     fprintf(fp, "%d", money);
 
     fclose(fp);
+}
+void playMiniGame() {
+      int target;
+    int choice;
+    int attempts = 2;
+
+    srand(time(0));
+
+    target = rand() % 5 + 1;
+
+    printf("=================================\n");
+    printf("      Duwa Gamay Daog Dako      \n");
+    printf("          Mystery Box      \n");
+    printf("=================================\n");
+    printf("Choose the right one!\n");
+    printf("You have 2 attempts.\n\n");
+
+    while (attempts > 0) {
+
+        printf("Boxes:\n");
+        printf("[1] [2] [3] [4] [5]\n");
+        printf("Choose a box number: ");
+        scanf("%d", &choice);
+
+        if (choice == target) {
+            printf("\n🎉 CONGRATULATIONS! 🎉\n");
+            printf("You won a reward!\n");
+            printf("-> Free Candy / Discount Coupon\n");
+            break;
+        } else {
+            attempts--;
+            if (attempts > 0) {
+                printf("\nWrong box!\n");
+                printf("You have %d attempts left.\n\n", attempts);
+            } else {
+                printf("\nGame Over!\n");
+                printf("Better luck next time bro!\n");
+                printf("Correct box was: %d\n", target);
+            }
+        }
+    }
+
+    printf("\nThank you for playing!\n");
+
+}
+void aboutOwner() {
+    while(1){
+    int choice;
+    printf("\n===== ABOUT THE PBCT OWNER =====\n");
+    printf("Name: Rodel E. Tan, Patricia Obejas, Rhealyn Cabarloc, and NIzzah Bejer\n");
+    printf("Student ID: 2025025445, 2025025446, 2025025447, 2025025448\n");
+    printf("Course: Associate in Computer Technology\n");
+    printf("Currently studying at San Jose Recoleto\n");
+    printf("Our Goal: To provide interactive and engaging vending machine experiences for our customers\n");
+    printf("Barato na may tsansa pang manalo ka!\n");
+
+    printf("=================================\n");
+    printf("1. View Contact Info\n");
+    printf("2. Back to Main Menu\n");
+    printf("Choose an option: ");
+    scanf("%d", &choice);
+    if (choice == 1) {
+
+        printf("\n=================================\n");
+        printf("         CONTACT DETAILS         \n");
+        printf("=================================\n");
+
+        printf("Email    : rodeltangwapo28@gmail.com\n");
+        printf("Phone    : 0962-825-6419\n");
+        
+        break;
+
+    } else if (choice == 2) {
+        printf("\nReturning to Main Menu...\n");
+        break;
+    } else {
+        printf("\nInvalid choice.\n");
+    }
+}
 }
